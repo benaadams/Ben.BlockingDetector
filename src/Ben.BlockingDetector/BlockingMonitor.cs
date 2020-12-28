042b17dfc16addcb69bb10b3165740f78f009f8a
Gabe Brown <gabe@bigboxvr.com>
2020-12-28 15:41:50 -0800

    BlockingMonitor: Added callback with stack trace to allow users to control the flow

--- src/Ben.BlockingDetector/BlockingMonitor.cs
+++ src/Ben.BlockingDetector/BlockingMonitor.cs
@@ -5,12 +5,22 @@ using Microsoft.Extensions.Logging;
 
 namespace Ben.Diagnostics
 {
-    internal class BlockingMonitor
+    public class BlockingMonitor
     {
         [ThreadStatic]
         private static int t_recursionCount;
 
         private readonly ILogger _logger;
+
+        /// <summary>
+        /// Gets or sets if the monitor should log blocking method calls
+        /// </summary>
+        public static bool LogBlockingMethodCalls { get; set; } = true;
+
+        /// <summary>
+        /// Event that is dispatched when a blocking method is detected
+        /// </summary>
+        public static event Action<StackTrace> BlockingMethodCalled;
 
         public BlockingMonitor(ILoggerFactory loggerFactory)
         {
@@ -31,8 +41,14 @@ namespace Ben.Diagnostics
             {
                 if (t_recursionCount == 1)
                 {
-                    _logger.BlockingMethodCalled(
-                        new StackTrace(dectectionSource == DectectionSource.SynchronizationContext ? 3 : 6));
+                    var stackTrace = new StackTrace(dectectionSource == DectectionSource.SynchronizationContext ? 3 : 6);
+
+                    if (BlockingMonitor.LogBlockingMethodCalls)
+                    {
+                        _logger.BlockingMethodCalled(stackTrace);
+                    }
+                    
+                    BlockingMonitor.BlockingMethodCalled?.Invoke(stackTrace);
                 }
             }
             catch
