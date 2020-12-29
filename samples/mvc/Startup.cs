0e9b3a327b1b0d18de284dafa05b98ad8a09d1af
Gabe Brown <gabe@bigboxvr.com>
2020-12-28 18:14:43 -0800

    Move example to samples project

--- samples/mvc/Startup.cs
+++ samples/mvc/Startup.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.IO;
 using Ben.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -16,6 +18,8 @@ namespace mvc
         {
             // Add blocking detector at start of Configure
             app.UseBlockingDetection();
+            BlockingMonitor.BlockingMethodCalled += OnBlockingMethodCalled;
+            BlockingMonitor.LogBlockingMethodCalls = false;
 
             if (env.IsDevelopment())
             {
@@ -23,6 +27,29 @@ namespace mvc
             }
 
             app.UseMvc();
+        }
+
+        private void OnBlockingMethodCalled(StackTrace stackTrace, int framesToSkip)
+        {
+            for (var x = framesToSkip - 1; x < stackTrace.FrameCount; x++)
+            {
+                var frame = stackTrace.GetFrame(x);
+
+                if (!string.IsNullOrEmpty(frame.GetFileName()))
+                {
+                    var fileName = Path.GetFileName(frame.GetFileName());
+                    var blockingCall = string.Empty;
+                    var line = frame.GetFileLineNumber();
+
+                    if (frame.HasMethod())
+                    {
+                        blockingCall = frame.GetMethod().Name;
+                    }
+
+                    Debug.WriteLine($"Blocking Call {blockingCall} on {fileName}:{line}");
+                    break;
+                }
+            }
         }
     }
 }
